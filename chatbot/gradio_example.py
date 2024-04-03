b8ed55d8283f6725258d65655e139a8069a23043
mrazelu2 <mrazelu2@fit.cvut.cz>
2024-04-03 15:54:09 +0200

    Yannick offline zmeny

--- chatbot/gradio_example.py
+++ chatbot/gradio_example.py
@@ -10,29 +10,30 @@ def send_message(msg):
 
 def main():
 
-    CHAT_GPT = False
+    CHAT_GPT = True
     if CHAT_GPT:
         client = openai.Client(api_key=os.environ["OPENAI_API_KEY"])
         chbot = chatbot.Chatbot(client, "gpt-4")
         def our_response(message, history):
             bot_msg = chbot.chat(message)
+            #send_message(bot_msg)
             return bot_msg
     else:
         
         def our_response(message, history):
             bot_msg = f"Your message was {message}"
-            send_message(bot_msg)
+            message += ". (Odpověz max. ve 3 větách)"
             return bot_msg
     
 
     demo = gr.ChatInterface(
-        our_response,
+        fn=our_response,
+        chatbot=gr.Chatbot(value=[[None, "Ahoj, jsem virtuální poradce a jsem tu abych ti pomohl si vybrat obor na FITu"]]),
         multimodal=False,
         undo_btn=None,
         retry_btn=None,
         clear_btn=None,
         title="Robot Pepper",
-        theme="dark"
     )
     demo.launch()
 
