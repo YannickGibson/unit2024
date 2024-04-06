de64fa0b327a6bc313c3caeef1139451be2007a8
Yannick Daniel Gibson <gibsoyan@fit.cvut.cz>
2024-04-06 21:59:20 +0200

    wifi outage commit

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 
 
 Gradio is run on http://127.0.0.1:7860/?__theme=dark
+
+
+
+## Example discussion for student:
+Ahoj chtěl bych se zeptat jaký 
+
+Chtěl bych pomoct s výběrem specializace
+
+Zajímá mě assembly a hraju si s FPGA
+
+😡
+
+
+
+
+
+
+http://127.0.0.1:7860/?__theme=dark
--- chatbot/gradio_example.py
+++ chatbot/gradio_example.py
@@ -4,37 +4,53 @@ import os
 import chatbot
 import subprocess
 
-def send_message(msg):
+def send_to_pepper(msg):
     ssh_command = f"ssh nao@172.20.10.103 -i id_pepper \"/home/nao/DumpIT/run.sh '{msg}'\""
     subprocess.run(ssh_command, shell=True, check=True)
 
 def main():
 
-    CHAT_GPT = False
+    CHAT_GPT = True
+    SEND_TO_PEPPER = False
+
     if CHAT_GPT:
+        print(os.environ["OPENAI_API_KEY"])
+        exit()
         client = openai.Client(api_key=os.environ["OPENAI_API_KEY"])
         chbot = chatbot.Chatbot(client, "gpt-4")
-        def our_response(message, history):
-            bot_msg = chbot.chat(message)
-            return bot_msg
-    else:
         
-        def our_response(message, history):
+    def our_response(message, history):
+        print("XXX Input XXX")
+        message += ". (Odpověz maximálně ve 3 větách.)"
+        print(message)
+
+        if CHAT_GPT:
+            bot_msg = chbot.chat(message)
+        else:
             bot_msg = f"Your message was {message}"
-            send_message(bot_msg)
-            return bot_msg
+
+        print("XXX Output XXX")
+        print(bot_msg)
+
+        if SEND_TO_PEPPER:
+            send_to_pepper(bot_msg)
+
+        return bot_msg
+    
     
 
     demo = gr.ChatInterface(
-        our_response,
+        fn=our_response,
+        chatbot=gr.Chatbot(value=[[None, "Ahoj, jsem virtuální asistent a jsem tu abych ti pomohl s výběrem oboru na FITu (Fakulta Informačních Technologií)."]]),
         multimodal=False,
         undo_btn=None,
         retry_btn=None,
         clear_btn=None,
         title="Robot Pepper",
-        theme="dark"
     )
-    demo.launch()
+    # launch online
+    demo.launch(share=False)
+    #demo.launch()
 
 if __name__ == "__main__":
     main()
